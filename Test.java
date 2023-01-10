44068ecf6b934fe0536139c78186e16d2afb0583
yadavkiranrr <117455342+yadavkiranrr@users.noreply.github.com>
2023-01-10 10:06:03 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+package com.jspider.multithreding;
+
+public class Test extends Thread{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread th = new Thread();
+		th.run();
+    
+	}
+
+}
---
+++ Thread.java
@@ -0,0 +1,11 @@
+package com.jspider.multithreding;
+
+public class Thread {
+	
+	public void run() {
+		System.out.println("thred created success");
+		
+
+	}
+
+}
